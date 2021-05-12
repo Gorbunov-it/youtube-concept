9f3c59c1f2c485337aee36ad8e1a2c1150ae1942
Gorbunov-it <82673512+Gorbunov-it@users.noreply.github.com>
2021-05-12 23:31:00 +0400

    Исправил меню и шапку. Добавил контент

--- js/main.js
+++ js/main.js
@@ -5,7 +5,38 @@ const swiper = new Swiper('.channel-slider', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  }
+});
+
+const recommended_swiper = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+
+    1100: {
+      slidesPerView: 2
+    },
+  } ,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  }
+});
+
+const foodDrink_swiper = new Swiper('.foodDrink-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.foodDrink-button-next',
+    prevEl: '.foodDrink-button-prev',
   }
 });
