71153d67a470868116669283bc4b167a09012590
Gorbunov-it <82673512+Gorbunov-it@users.noreply.github.com>
2021-05-13 01:59:50 +0400

    Добавил правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
@@ -13,12 +30,12 @@ const swiper = new Swiper('.channel-slider', {
 const recommended_swiper = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
   breakpoints: {
     1600: {
       slidesPerView: 3
     },
-
     1100: {
       slidesPerView: 2
     },
@@ -33,10 +50,40 @@ const recommended_swiper = new Swiper('.recommended-slider', {
 const foodDrink_swiper = new Swiper('.foodDrink-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.foodDrink-button-next',
     prevEl: '.foodDrink-button-prev',
   }
 });
+
+const searchButton = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchButton.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  recommended_swiper.destroy();
+  foodDrink_swiper.destroy();
+}
